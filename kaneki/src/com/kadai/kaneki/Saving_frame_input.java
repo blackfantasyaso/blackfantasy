47c487fc119a095772cb6ac385caf26c4746dcba
aso1301007 <1301007@st.asojuku.ac.jp>
2015-06-09 11:23:47 +0900

    commit

---
+++ kaneki/src/com/kadai/kaneki/Saving_frame_input.java
@@ -0,0 +1,14 @@
+package com.kadai.kaneki;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Saving_frame_input extends Activity {
+
+	 @Override
+	    public void onCreate(Bundle savedInstanceState) {
+	        super.onCreate(savedInstanceState);
+	        setContentView(R.layout.saving_frame_input);
+	    }
+
+}
