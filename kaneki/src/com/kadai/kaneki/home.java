20f5f9f534143ffa7efe599a1b451b95aae361fa
aso1301023 <aso1301023@st.asojuku.ac.jp>
2015-06-12 10:35:35 +0900

    fgh

--- kaneki/src/com/kadai/kaneki/home.java
+++ kaneki/src/com/kadai/kaneki/home.java
@@ -22,7 +22,7 @@ public class home extends Activity {
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
+		// Handle action bar item clicks here. The action bar will qa
 		// automatically handle clicks on the Home/Up button, so long
 		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
